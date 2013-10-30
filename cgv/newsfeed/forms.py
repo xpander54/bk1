8978632ab5459d4103887f33f2ef54acb3bd2a2c
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-10-29 18:18:55 -0600

    Se crea forma para subscripción de newsfeed

--- cgv/newsfeed/forms.py
+++ cgv/newsfeed/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from newsfeed.models import Subscriptor
 
-class IndexFeedForm(ModelForm):
+class EmailNewsfeedForm(ModelForm):
 	class Meta:
 		model  = Subscriptor
 		fields = ('email',)
