2aef4dded3594fea699ec5477e729d92e5af0bc1
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-08 15:56:21 -0600

    Vistas de la aplicacion blog
    Se crea el modulo de vistas "blog/views.py" para la aplicación blog.
    
    Se crea vista de reflexiones.

--- cgv/blog/views.py
+++ cgv/blog/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.shortcuts import render
+from newsfeed.forms import EmailNewsfeedForm
+from blog.models import Post
+
+
+def reflexiones(request):
+
+	forma_suscripcion = EmailNewsfeedForm()
+	reflexiones       = Post.objects.filter(categoria__nombre = 'reflexiones', estado = 'p')
+	audio             = reflexiones.filter(tags__nombre = 'audio')
+	video             = reflexiones.filter(tags__nombre = 'video')
+	ebook             = reflexiones.filter(tags__nombre = 'ebook')
+	context           = {
+		'audio' : audio,
+		'video' : video,
+		'ebook' : ebook,
+	}
+
+
+	return render(request, 'blog/reflexiones.html', context)
+
+
