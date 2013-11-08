504f311248a377db41e0443376d1e32d25ccbf30
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-08 13:31:47 -0600

    Cambios temportales a las rutas

--- cgv/cgv/urls.py
+++ cgv/cgv/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import views
+
 
 
 admin.autodiscover()
@@ -8,4 +10,6 @@ urlpatterns = patterns('',
 	url(r'^', include('newsfeed.urls')),
 	url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^Contacto/$', views.contacto, name = 'contacto'),
+    url(r'^Reflexiones/$', views.reflexiones, name = 'reflexiones'),
 )
