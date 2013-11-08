0d3c5e66c398cfb8346d1b9554ea8017db2924ea
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-08 17:44:07 -0600

    se corrije error en el sync del branch

--- cgv/cgv/urls.py
+++ cgv/cgv/urls.py
@@ -10,11 +10,6 @@ urlpatterns = patterns('',
 	url(r'^', include('newsfeed.urls')),
 	url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'^blog/', include('blog.urls')),
     url(r'^Contacto/$', views.contacto, name = 'contacto'),
-=======
-    url(r'^Contacto/$', views.contacto, name = 'contacto'),
-    url(r'^Reflexiones/$', views.reflexiones, name = 'reflexiones'),
->>>>>>> e09a111e1f382d125573b939a59851b9ddba77db
 )
