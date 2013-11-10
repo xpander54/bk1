d3f7abd8a8860b296bb87d51d76d356c2a5d5030
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-09 18:47:01 -0600

    Se añade Custom View para envio del newsfeed y su template

--- cgv/newsfeed/admin.py
+++ cgv/newsfeed/admin.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+from django.conf.urls import patterns
 from django.contrib import admin
 from newsfeed.models import Feed, Subscriptor
 
@@ -5,6 +8,23 @@ from newsfeed.models import Feed, Subscriptor
 class SubscriptorAdmin(admin.ModelAdmin):
 
 	list_display = ('__unicode__', 'activo', 'codigo', 'creado')
+
+	def get_urls(self):
+
+		urls    = super(SubscriptorAdmin, self).get_urls()
+		my_urls = patterns('',
+			(r'^my_view/$', self.admin_site.admin_view(self.my_view)),
+		)
+
+		return my_urls + urls
+
+	def my_view(self, request):
+
+		if request.method == 'POST':
+
+			return HttpResponse('ES POST!')
+
+		return render(request, 'newsfeed/admin/my_view.html')
 	
 
 
