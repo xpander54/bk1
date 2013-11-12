25435c3ecebb7d1d74fc72d6e50f9259b32acfc2
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-11 18:18:19 -0600

    Aplicación Archivos

---
+++ cgv/archivos/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from archivos.models import Categoria, Archivo
+
+
+class ArchivosInline(admin.TabularInline):
+
+	model           = Archivo
+	extra           = 1
+	fields          = ('link', 'archivo', 'creado')
+	readonly_fields = ('link', 'creado',)
+
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+
+	inlines = [ArchivosInline]
+
+
+admin.site.register(Categoria, CategoriaAdmin)
