3636f7c3d7560aad8ad2ff9711d7fd276913f785
xpander54 <rodrigo@holbox.in>
2013-11-11 21:35:55 -0600

    contacto

--- cgv/views.py
+++ cgv/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from newsfeed.models import Subscriptor
 from newsfeed.forms import EmailNewsfeedForm
+from django.http import HttpResponseRedirect, HttpResponse
+from cgv.forms import ContactForm
 
 def contacto(request):
 	return render(request, 'noticias/contacto.html')
@@ -12,3 +14,19 @@ def reflexiones(request):
 	}    
 
 	return render(request, 'noticias/reflexiones.html', context)
+
+def contacto(request):
+
+	if request.method == 'POST':
+		forma_contacto = ContactForm(request.POST)
+
+	else:
+		forma_contacto = ContactForm()
+
+	context={
+		'forma_contacto' : forma_contacto,
+	}
+
+	return render(request, 'contacto.html', context)
+    
+
