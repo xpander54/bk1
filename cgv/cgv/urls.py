1729fc9bd3bf0d8bbaae3917063811380bb41b88
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-11 18:17:59 -0600

    URL Media

--- cgv/cgv/urls.py
+++ cgv/cgv/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 import views
 
@@ -12,4 +14,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
     url(r'^Contacto/$', views.contacto, name = 'contacto'),
-)
+) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
