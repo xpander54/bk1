429f16445c6a8a3f02f1c8c1a49c6db4c640ecb7
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2013-11-15 17:27:30 -0600

    Se definen nuevos urls
    Se cambia el patron url contacto "Contacto" por "contacto"
    Se modifica url login

--- cgv/cgv/urls.py
+++ cgv/cgv/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-import views
+from cgv import views
 
 
 
@@ -14,9 +14,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^galerias/', include('galerias.urls')),
     url(r'^blog/', include('blog.urls')),
-    url(r'^Contacto/$', views.contacto, name = 'contacto'),
-    url(r'^login/$', views.login, name = 'login'),
+    url(r'^contacto/$', views.contacto, name = 'contacto'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name' : 'auth/login.html'}),
 	url(r'^registro/$', views.registro, name = 'registro'),    
 	url(r'^registro/$', views.registro, name = 'registro'),
-)+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
