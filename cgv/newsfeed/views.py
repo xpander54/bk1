9eef2a5b2c7e4e537baeead9a6b06777752ff166
xpander54 <rodrigo@holbox.in>
2014-05-05 13:16:02 -0500

    mail bienvenida gabyvaldesh@gmail.com

--- cgv/newsfeed/views.py
+++ cgv/newsfeed/views.py
@@ -4,6 +4,9 @@ import string
 import random
 
 from django.core.mail import send_mail, BadHeaderError
+
+from django.core.mail import EmailMultiAlternatives
+
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -29,7 +32,7 @@ def index(request):
 			suscriptor.codigo = __generate_code()
 			# PASAR ESTO AL MODELO <
 
-			email_subject = "Confrimación de la subscripción Neewsletter Gaby Valdes"
+			email_subject = "Confrimación de la subscripción Newsletter Gaby Valdes"
 			email_message = """
 				Para confirmar su subscripción al neewsletter visite la siguiente liga:
 
@@ -37,9 +40,14 @@ def index(request):
 			""" % suscriptor.codigo
 			email_to     = suscriptor.email
 
+			
+
+			
+			
 			if __send_email(email_subject, email_message, email_to):
 
 				suscriptor.save()
+
 				return HttpResponseRedirect('/')
 				
 			else:
@@ -78,7 +86,16 @@ def newsfeed_confirm(request, code):
 		suscriptor.activo = True
 		suscriptor.save()
 
-		return HttpResponse('Correo Confirmado.')
+		email_to     = suscriptor.email
+
+		subject, from_email, to = 'Bienvenido a Gaby Valdes Newsletter', 'from@example.com', [email_to]
+		text_content = 'Bienvenido a Gaby Valdes, para leer este correo, necesitas un lector que soporte HTML'
+		html_content = '<p>Gracias por permitirme ser parte de tu comunidad!</p><p>Te doy la bienvenida a este sitio donde podremos compartir infinidad de temas de inter&eacute;s, estoy aqu&iacute; para entender y atender tus inquietudes, para que podamos juntos encontrar mejores maneras de relacionarnos con los dem&aacute;s y con nosotros mismos. S&eacute; que hay muchas cosas importantes en tu vida, y que parecer&iacute;a que el tiempo se hace mas corto y el mundo mas peque&ntilde;o, por lo que a veces puedes sentirte confundido en temas vitales: pareja, hijos, dinero, &eacute;xito, trabajo, espiritualidad, salud, superaci&oacute;n personal&#8230;por eso estoy aqu&iacute;, contigo y para ti, para escuchar tus necesidades y brindarte las herramientas adecuadas. Ahora que eres parte de esta comunidad vas a recibir semanalmente informaci&oacute;n y herramientas muy &uacute;tiles, por lo que te sugiero que dediques unos minutos a revisar el material que recibir&aacute;s.</p><p>Mi inter&eacute;s es darte sugerencias f&aacute;ciles, que no te quiten mucho tiempo, por lo que encontraras contenido claro y conciso. Si&eacute;ntete libre de compartir la informaci&oacute;n que aqu&iacute; recibes de manera gratuita, y por favor deja tus comentarios, ya que el material que se env&iacute;a esta basado en los intereses de la propia comunidad. &iquest;Cuales son tus gustos? &iquest;Que temas te apasionan? &iquest;Que quieres aprender? Estoy segura que siempre es un buen momento para estar mejor y disfrutar de la vida, mas all&aacute; de las complicaciones del momento, y s&eacute; que si est&aacute;s aqu&iacute; conmigo es porque tienes ganas de encontrar mayor plenitud y armon&iacute;a.</p><p>Felicidades!! Estoy convencida de que las casualidades no existen, si nos hemos encontrado en este momento ser&aacute;, seguramente para algo constructivo, y deseo que esta relaci&oacute;n que hoy iniciamos sea muy fruct&iacute;fera y llena de satisfacciones. Un abrazo donde quiera que est&eacute;s, estamos en contacto permanente, y, de nuevo,</p><p>BIENVENIDO!!</p><p>Gaby Vald&eacute;s</p><p><a href="http://www.gabyvaldes.com">www.gabyvaldes.com</a></p><p><a href="mailto:contacto@gabyvaldes.com">contacto@gabyvaldes.com</a></p><p><img src="http://cursos.gabyvaldes.com/wp-content/uploads/2014/03/logo-login.png" /></p>'
+		msg = EmailMultiAlternatives(subject, text_content, from_email, [email_to])
+		msg.attach_alternative(html_content, "text/html")
+		msg.send()
+
+		return HttpResponse('Correo Confirmado. <a href="http://www.gabyvaldes.com">regresar</a>')
 
 	return HttpResponse('El correo ya había sido confirmado.')
 
@@ -112,6 +129,8 @@ def newsfeed_send_code(request):
 				email_message = """
 					http://gabyvaldes.com/%s/newsfeed-deactivate/
 				""" % suscriptor.codigo
+
+
 
 				if __send_email(email_subject, email_message, correo):
 
